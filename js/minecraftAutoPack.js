79fc0f7f1cb5d4a962b7a4b732a0766bfafbf4b0
Lepeltier kévin <kevin@lepeltier.info>
2015-02-22 18:19:46 +0100

    percent

--- js/minecraftAutoPack.js
+++ js/minecraftAutoPack.js
@@ -147,9 +147,10 @@ Zepto(function($){
                     var pack = vers.variant[j];
                     pack.version = pack.version || '';
                     pack.link = pack.link || '';
+                    var percent = pack.percent ? ' ('+pack.percent+'%)' : '';
                     var id = '_'+i+'_'+j;
                     var link = pack.link != '' ? ' -> <a href="'+pack.link+'">site</a>' : '';
-                    var li = $('<li class="dl" style="background-image: url('+pack.img+')"><input id="input'+id+'" '+(j==0?'checked="checked"':' ')+'type="checkbox" name="list" value="'+j+'"/><label for="input'+id+'">'+pack.name+' '+pack.version+link+'</label></li>');
+                    var li = $('<li class="dl" style="background-image: url('+pack.img+')"><input id="input'+id+'" '+(j==0?'checked="checked"':' ')+'type="checkbox" name="list" value="'+j+'"/><label for="input'+id+'">'+pack.name+' '+pack.version+percent+link+'</label></li>');
                     
                     ul.append(li);
                 }
